57b541226ccd313bf303e39bd3df25a1c909f4d8
Konstantin <kolupaevkonstantin18@gmail.com>
2022-12-07 21:34:29 +0700

    created two pages home and rental

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const onClickPrice = () => {
+  const buttonCard = document.querySelector('.modal');
+  buttonCard.classList.toggle('none');
+};
+
+const closeModal = () => {
+  const close = document.querySelector('.modal');
+  close.classList.toggle('none');
+};
